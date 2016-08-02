52491137a4685eb74f4bdcd30635ebd4a641eb3d
Ryo Suzuki <ryosuzk@gmail.com>
2016-08-02 11:01:37 -0700

    Convert black and white frame

--- video.py
+++ video.py
@@ -1,9 +1,22 @@
 import cv2
+import sys, getopt
 
-def main():
-  cap = cv2.VideoCapture('./video/invader.mp4')
+pixel_count = 52
+file = './video/invader.mp4'
+
+def main(argv):
+  if (argv and argv[0]):
+    global file
+    file = argv[0]
+  if (argv and argv[1]):
+    global pixel_count
+    pixel_count = int(argv[1])
+  stream()
+
+def stream():
+  cap = cv2.VideoCapture(file)
   while not cap.isOpened():
-    cap = cv2.VideoCapture('./video/invader.mp4')
+    cap = cv2.VideoCapture(file)
     cv2.waitKey(1000)
     print 'Wait for the header'
 
@@ -12,9 +25,9 @@ def main():
     flag, frame = cap.read()
     if flag:
       # The frame is ready and already captured
-      processFrame(frame)
+      process(frame)
       pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-      print str(pos_frame)+' frames'
+      print str(pos_frame) + ' frames'
     else:
       # The next frame is not ready, so we try to read it again
       cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
@@ -29,15 +42,32 @@ def main():
       # we stop
       break
 
-def processFrame(img):
+def process(img):
   original_size = img.shape[:2][::-1]
-  output_size = (26, 26)
-  img2 = cv2.resize(img, output_size)
-  img3 = cv2.resize(img2, original_size, interpolation=cv2.cv.CV_INTER_NN)
-  cv2.imshow('video', img3)
+  output_size = (pixel_count, pixel_count)
+  img_small = cv2.resize(img, output_size)
+  # Grayscale
+  img_gray = cv2.cvtColor(img_small, cv2.COLOR_RGB2GRAY)
+  # Black and white
+  (thresh, img_bw) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+  img_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+  # Output with original size
+  img_out = cv2.resize(img_bw, original_size, interpolation=cv2.cv.CV_INTER_NN)
+  cv2.imshow('video', img_out)
 
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1:])
+
+
+
+
+
+
+
+
+
+
+
 
 
 
